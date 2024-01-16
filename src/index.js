1617fa54447bb2bb47d45b836981b44e11f5adab
Mauricio <stb539@gmail.com>
2024-01-16 13:07:54 -0300

    vars /users/#

--- src/index.js
+++ src/index.js
@@ -8,21 +8,23 @@ const app = express()
 // middlewares
 app.use(responseTime())
 
+// Connecting to dock-redis or to redis
 const client = await createClient()
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect()
 
 app.get("/", async (req, res) => {
+  res.send("/users on redis")
+})
+
+app.get("/users", async (req, res) => {
   // const value = await client.get("key")
   const value = await client.get("usuarios")
   if (value) {
-    console.log("value")
-    console.log(JSON.parse(value)) // transf str a json
     return res.json(JSON.parse(value))
   }
 
   const response = await axios.get("https://jsonplaceholder.typicode.com/users")
-  console.log('api')
 
   // await client.set("key", "value") // guardar variable
 
@@ -31,6 +33,33 @@ app.get("/", async (req, res) => {
     JSON.stringify(response.data) // transf json a str
   )
   res.json(response.data)
+})
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    // buscar en redis
+    const value = await client.get(req.originalUrl)
+    if (value) {
+      return res.json(JSON.parse(value))
+    }
+
+    // buscar en api
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users/" + req.params.id
+    )
+
+    await client.set(req.originalUrl, JSON.stringify(response.data))
+
+    return res.json(response.data)
+  } catch (error) {
+    return res.status(error.response.status).json({ msj: error.message })
+  }
+})
+
+//error 404
+app.use("*", function (req, res, next) {
+  res.status(404).json({ msj: "route not found" })
+  next()
 })
 
 app.listen(3000)
